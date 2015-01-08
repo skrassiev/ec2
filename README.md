02f1edcbf82add8dc25c95bff7b37c6536508068
Sasha Krassiev <alexander.krassiev@citrix.com>
2015-01-08 14:31:34 -0800

    user init done

--- README.md
+++ README.md
@@ -2,4 +2,5 @@ Run with:
 
 ```
 curl -sSL https://raw.githubusercontent.com/skrassiev/ec2/master/init.sh | sudo sh
+curl -sSL https://raw.githubusercontent.com/skrassiev/ec2/master/userinit.sh | sh
 ```
--- userinit.sh
+++ userinit.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+#pull test sources
 mkdir -p code/github
 cd code/github
 git clone https://github.com/skrassiev/golang-docker-test-webbench.git
 cd golang-docker-test-webbench
+
+
+#pull docker test image
+sudo docker run -ti --rm  --publish=6060:8080 skrassiev/golang-docker-test-webbench:latest
