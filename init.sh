0a969e674c15a5e1fa8089b678715fec21bdc212
Sasha Krassiev <alexander.krassiev@citrix.com>
2015-06-04 22:54:34 -0700

    docker-compose 1.2 fails to pull from a private registry with authentication

--- init.sh
+++ init.sh
@@ -44,7 +44,7 @@ fi
 cat /proc/$(ps auxww|grep docker|grep -v grep | awk '{print $2}')/limits
 
 #install Docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.1.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 echo Initialization Done
