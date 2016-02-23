98bb21a174eecf6955dafa6fd14336756d51f538
skrassiev <bg2128506@gmail.com>
2016-02-23 10:56:04 -0800

    install docker 0.10 on ubu

---
+++ init_ubuntu_docker010.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+echo Initializing EC2 Amazon Linux or EC2 Ubuntu instance for golang and docker
+
+OS=$(grep '^NAME=' /etc/os-release | awk -F\" '{print $2}')
+OS1=$(cat /etc/redhat-release | awk '{print $1 $2 $3}')
+
+#package manager
+if [ "$OS" = "Ubuntu" ]; then
+	PKGM=apt-get
+	OPENSSL_DEV=libssl-dev 
+else
+	PKGM=yum
+	OPENSSL_DEV=openssl-devel.x86_64
+fi
+
+#update dist
+$PKGM update -y
+
+# install Ubuntu per https://docs.docker.com/engine/installation/linux/ubuntulinux/
+$PKGM install -y apt-transport-https ca-certificates
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
+$PKGM update
+$PKGM purge lxc-docker
+$PKGM install -y docker-engine nmap net-tools ngrep
+service docker start
+usermod -aG docker ubuntu
+
+# #install packages we need
+# $PKGM install -y gcc git $OPENSSL_DEV
+
+# #increase ulimits
+# echo "*               hard    nofile          65536" >>  /etc/security/limits.conf
+# echo "*               soft    nofile          65536" >>  /etc/security/limits.conf
+
+# #install and configure go
+# curl -sSL https://storage.googleapis.com/golang/go1.5.linux-amd64.tar.gz | tar -C /usr/local -xvzf -
+# echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
+
+# #install docker
+# if [ "$OS" = "Ubuntu" ]; then
+# 	curl -sSL https://get.docker.com/ubuntu/ | sh
+# 	usermod -aG docker ubuntu
+# 	service docker start
+# elif [ "$OS1" = "RedHatEnterprise" ]; then
+# 	$PKGM install -y docker-io --enablerepo=epel
+# 	service docker start
+# 	chkconfig docker on
+# else
+# 	$PKGM install -y docker-io
+# 	service docker start
+# 	chkconfig docker on
+# fi
+
+# cat /proc/$(ps auxww|grep docker|grep -v grep | awk '{print $2}')/limits
+
+# #install Docker-compose
+# curl -L https://github.com/docker/compose/releases/download/1.4.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+# chmod +x /usr/local/bin/docker-compose
+
+# echo Initialization Done
+# echo You need to reboot for ulimit to take effect
+# echo After reboot, restart docker with 'sudo service docker restart'
+
